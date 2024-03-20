0a9f19898d2883249889308ecbdba8ee866b8369
Anmol Pandey <87567652+AnmolPandey06@users.noreply.github.com>
2024-03-20 15:34:11 +0530

    created the server

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify, json, render_template
+
+app = Flask(__name__)
+
+data = dict()
+
+@app.route('/server', methods=['GET', 'POST'])
+def showRequest():
+    def storeRequest():
+        data["req_data"] = request_data
+        data["req_header"] = req_header
+        data["req_method"] = req_method
+
+    if (request.is_json):
+        request_data = request.json
+    else:
+        request_data = "no json data in request"
+    req_header = request.headers
+    req_method = request.method
+
+    storeRequest()
+
+    return jsonify(request_data)
+
+
+
+@app.route('/showData', methods=['GET'])
+def showData():
+
+    return render_template("show_request.html", 
+                        request_data = data["req_data"],
+                        req_header = data["req_header"],
+                        req_method = data["req_method"])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=105)
