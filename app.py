f6b3d245b9031f8744b2c5c075de277fab8e931d
Anmol Pandey <anmol-kumar-pandey.anmol-kumar-pandey@hp.com>
2024-03-26 12:42:03 +0530

    Added the binary decoder for hex data

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ app = Flask(__name__)
 
 data = dict()
 
+def decode(req_data):
+    hex_data = req_data
+    
+    if hex_data:
+        b_data = bytes(hex_data)
+        return b_data
+
+    return "No hex_data"
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template("index.html")
@@ -14,6 +23,7 @@ def showRequest():
         data["req_data"] = request_data
         data["req_header"] = req_header
         data["req_method"] = req_method
+        data["req_bin_data"] = binary_data
 
     if (request.is_json):
         request_data = request.json
@@ -21,6 +31,7 @@ def showRequest():
         request_data = request.data
     req_header = request.headers
     req_method = request.method
+    binary_data = decode(request_data)
 
     storeRequest()
 
@@ -34,8 +45,9 @@ def showData():
     return render_template("show_request.html", 
                         request_data = data["req_data"],
                         req_header = data["req_header"],
-                        req_method = data["req_method"])
+                        req_method = data["req_method"],
+                        binary_data = data['req_bin_data'])
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+    app.run(host='0.0.0.0', port=5000)
